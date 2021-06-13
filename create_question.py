1d702c57d67650ec05b76182358036491f289dc9
Owen McCadden <omccadden@artistrepublik.com>
2021-06-13 00:10:25 -0400

    initial setup

---
+++ create_question.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+from datetime import datetime
+#That's the lambda handler, you can not modify this method
+# the parameters from JSON body can be accessed like deviceId = event['deviceId']
+def lambda_handler(event, context):
+    # Instanciating connection objects with DynamoDB using boto3 dependency
+    dynamodb = boto3.resource('dynamodb')
+    client = boto3.client('dynamodb')
+    
+    # Getting the table the table Temperatures object
+    questions_table = dynamodb.Table('questions')
+    
+    # Getting the current datetime and transforming it to string in the format bellow
+    event_timestamp = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+    question_id = event['question_id']
+    question = event['question']
+    responses = []
+    
+    # Putting a try/catch to log to user when some error occurs
+    try:
+        
+        questions_table.put_item(
+           Item={
+                'event_timestamp': event_timestamp,
+                'question_id': question_id,
+                'question': question,
+                'responses': responses
+            }
+        )
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Succesfully inserted temperature!')
+        }
+    except:
+        print('Closing lambda function')
+        return {
+                'statusCode': 400,
+                'body': json.dumps('Error saving the temperature')
+        }
+
+
+
