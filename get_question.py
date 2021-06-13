4a48c7e8023f6f1b973ef22bac69bba2ee21e539
Owen McCadden <omccadden@artistrepublik.com>
2021-06-13 12:50:19 -0400

    added get_question lambda method

--- get_question.py
+++ get_question.py
@@ -1,12 +1,37 @@
 import json
 import boto3
+
 def lambda_handler(event, context):
-  
-  dynamodb = boto3.resource('dynamodb')
-  questions_table = dynamodb.Table('questions')
-  response = questions_table.scan()
-  
-  return {
-    'statusCode': 200,
-    'body': response
-  }
+    # Instanciating connection objects with DynamoDB using boto3 dependency
+    dynamodb = boto3.resource('dynamodb')
+    
+    # Getting the table the table questions object
+    questions_table = dynamodb.Table('questions')
+    
+    question_id = event['question_id']
+    
+    # Putting a try/catch to log to user when some error occurs
+    try:
+        
+        response = questions_table.get_item(
+           Key={
+               'question_id': question_id
+           }
+        )
+
+        if response['Item'] is None:
+            raise KeyError("Question_id {} does not exist in table.".format(event['question_id']))
+        
+        return {
+            'statusCode': 200,
+            'body': response
+        }
+
+    except:
+        print('Closing lambda function')
+        return {
+                'statusCode': 400,
+                'body': json.dumps('Error getting question with id {}'.format(question_id))
+        }
+
+lambda_handler({'question_id': '1'}, None)
