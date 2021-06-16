4b87b3026d5ffafcdaeaf03664a8d4a3f526412a
Owen McCadden <omccadden@artistrepublik.com>
2021-06-16 00:35:54 -0400

    updated response methods

--- create_response.py
+++ create_response.py
@@ -1,4 +1,62 @@
 import json
 import boto3
 def lambda_handler(event, context):
-  return
+  # Instanciating connection objects with DynamoDB using boto3 dependency
+    dynamodb = boto3.resource('dynamodb')
+    client = boto3.client('dynamodb')
+    
+    # Getting the table the table questions object
+    questions_table = dynamodb.Table('questions')
+    
+    # current datetime as a potential timestamp attribute
+    # event_timestamp = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+    question_id = event['question_id']
+    response = event['response']
+    
+    # Putting a try/catch to log to user when some error occurs
+    try:
+
+        # question = questions_table.get_item(
+        #     Key={
+        #        'question_id': question_id
+        #     }
+        # )
+
+        # response_id = len(question['Item']['responses']) + 1
+        
+        # question['Item']['responses'].append(
+        #   {
+        #     "response_id": response_id,
+        #     "response": response
+        #   }
+        # )
+
+        question = questions_table.get_item(
+          Key={
+            "question_id": question_id
+          }
+        )
+
+        existing_responses = question['Item']['responses']
+        existing_responses.append(response)
+
+        questions_table.update_item(
+          Key={
+               'question_id': question_id
+           },
+           UpdateExpression="SET responses = :responses",
+           ExpressionAttributeValues={
+            ':response': existing_responses,
+          }
+        )
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Succesfully created response!')
+        }
+    except:
+        print('Closing lambda function')
+        return {
+                'statusCode': 400,
+                'body': json.dumps('Error saving the question')
+        }
--- get_responses.py
+++ get_responses.py
@@ -1,4 +1,36 @@
 import json
 import boto3
+
 def lambda_handler(event, context):
-  return
+    # Instanciating connection objects with DynamoDB using boto3 dependency
+    dynamodb = boto3.resource('dynamodb')
+    
+    # Getting the table the table questions object
+    questions_table = dynamodb.Table('questions')
+    
+    question_id = event['question_id']
+    
+    # Putting a try/catch to log to user when some error occurs
+    
+    try:
+        
+        response = questions_table.get_item(
+           Key={
+               'question_id': question_id
+           }
+        )
+
+        if response['Item'] is None:
+            raise KeyError("Question_id {} does not exist in table.".format(event['question_id']))
+        
+        return {
+            'statusCode': 200,
+            'body': response['Item']['responses']
+        }
+
+    except:
+        print('Closing lambda function')
+        return {
+                'statusCode': 400,
+                'body': json.dumps('Error getting question with id {}'.format(question_id))
+        }
